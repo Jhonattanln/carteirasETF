adf156ef0804a6a5497d7e25994502c6c5941371
Jhonattan <jhonattanlino@outlook.com>
2024-08-25 22:02:26 -0300

    feat: replace null data

--- src/script/ETL/etl.py
+++ src/script/ETL/etl.py
@@ -1,7 +1,18 @@
-#%% Import data from excle file
+# Import data from excle file
+import datetime
+import numpy as np
 import pandas as pd
 
 # Read data from excel file
 df = pd.read_excel('src/data/etf.xlsx', parse_dates=True, index_col=0, skiprows=3)
-df.head()
-# %%
+
+## Transform data
+# Rename columns
+df.columns = df.columns.str[-6:]
+
+# Slice data
+data = datetime.datetime.today() - datetime.timedelta(days=365)
+df = df.loc[data:datetime.datetime.today().strftime('%Y-%m-%d')]
+
+# Replace data
+df.replace('-', np.nan, inplace=True)
