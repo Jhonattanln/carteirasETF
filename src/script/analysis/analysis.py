14901f05afc9192b9d52f3a1ea156561713ea430
Jhonattan <jhonattanlino@outlook.com>
2024-09-04 23:01:14 -0300

    feat: generate cleaned file

--- src/script/analysis/analysis.py
+++ src/script/analysis/analysis.py
@@ -16,18 +16,17 @@ class Analysis:
     def __init__(self):
         self.data = pd.read_excel('src/data/transformed_data.xlsx', parse_dates=True, index_col=0)
         self.droped = None
-        self.cleaned_data = None
-    
+
     def manipulate_data(self, cut=0.9):
         self.droped = self.data.dropna(how='all')
         count = cut * len(self.droped)
-        self.droped = self.droped.dropna(how='all', thresh=count)
+        self.droped = self.droped.dropna(axis=1, thresh=count)
         return self.droped
 
     def export_excel(self, file_path):
-        self.droped.to_excel(file_path)
+        self.droped.to_csv(file_path)
         return print('Arquivo exportado!')
 
 analysis = Analysis()
-analysis.drop_data()
-analysis.select_cut()
+analysis.manipulate_data()
+analysis.export_excel('src/data/etf.csv')
